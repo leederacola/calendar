fbdcfc8a4e4dc376b04302919789c6fe840a4a9c
Ryan <Ryan@CROCODILE>
2018-05-27 21:21:49 -0400

    css grid and some styling

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  numberDays: number;
+  startDate: string;
+  countryCode: string;
+  
+
+   
+
+
+
   constructor() { }
+
+
+
 
   ngOnInit() {
   }
