216921a8c6dc0eb6d5c0f3509fd607b24525c559
Ryan <Ryan@CROCODILE>
2018-05-27 20:49:49 -0400

    imports and mat style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatButtonModule} from '@angular/material/button'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatCardModule} from '@angular/material/card'
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule, MatFormFieldModule,
+    MatDatepickerModule, MatButtonModule,
+    MatCardModule, MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
